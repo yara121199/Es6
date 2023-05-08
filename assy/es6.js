3714e1b829f36b89bd32de27bb3472a9a9a85202
yara121199 <88503735+yara121199@users.noreply.github.com>
2023-05-08 03:14:56 +0300

    Add files via upload

---
+++ assy/es6.js
@@ -0,0 +1,127 @@
+/*var fru = ["banana","apple","mango"]
+for(var i in fru   )
+{
+
+  console.log(i)
+
+}
+
+var fru = ["banana","apple","mango"]
+for(var i of fru   )
+{
+
+  console.log(i)
+
+}*/
+
+
+/*___________________________________________________________________________*/
+/*
+var student = {
+
+   "name":"yara",
+    "University":"assuit",
+    "fuculty":"since",
+    "grade":50,
+
+    
+}
+console.log(`${student.name} is a student in faculty of ${student.fuculty} in university ${student.university}
+And his final grad is ${student.grade}.`);*/
+
+/*________________________________________________________________*/
+
+/*function getMax(...args)
+ {
+     let maxNumber= args[0];
+     for(let i=1;i<args.length;i++)
+     {
+         if(args[i]>maxNumber)
+         {
+             maxNumber=args[i];
+         }
+     }
+     return maxNumber;
+ }*/
+ /*____________________________________________________________________________*/
+
+ /*var s =new Set (["yara","sara","yara",..."taramohammed"])
+
+ for (var i of s)
+ {
+ 
+    console.log(i)
+
+ }*/
+ /*________________________________________________________________________________*/
+
+/* function grreting ()
+ {
+    alert("welcome to iti");
+   var p =prompt("please enter your faculty name")
+   console.log(p)
+   var pp = prompt("please enter department")
+   console.log(pp) 
+ }grreting ()*/
+ /*_______________________________________________________________________*/
+
+
+ /*let a =10;
+ let b=20;
+ [a,b]=[b,a];
+ console.log(a)
+ console.log(b)*/
+
+/* let a  
+ let b 
+ [a=3,b=9]=[9,3]
+ console.log(a)
+ console.log(b)*/
+
+
+ /*_____________________________________________________________________*/
+
+ /*function math()
+ {
+    return [
+
+       a+b,
+       (a+b)/2,
+       a*b
+    ]
+
+ }let[sum , avg , multi]=math(2,5)*/
+
+ /*_____________________________________________________________________*/
+
+ /*let arr1 =["yara","tara"]
+ let arr2 = ["mohammed","ali"]
+ let mergarray = [...arr1,...arr2]
+ console.log(mergarray)*/
+
+
+ /*_______________________________________________________________________-*/
+
+ /*let Grades = new Map();
+ Grades.set("John", {Grades:[{Coursename:"JavaScript", Grade:"Excellent"},{Coursename:"Jquery", Grade:"Good"},{Coursename:"CSS", Grade:"V.Good"}]});
+ Grades.set("Bob", {Grades:[{Coursename:"JavaScript", Grade:"Good"},{Coursename:"Jquery", Grade:"Excellent"},{Coursename:"CSS", Grade:"V.Good"}]});
+ studentGrades.set("Alice", {Grades:[{Coursename:"JavaScript", Grade:"V.Good"},{Coursename:"Jquery", Grade:"Excellent"},{Coursename:"CSS", Grade:"Good"}]});
+ 
+ for (let [key, value] of Grades) {
+   console.log(key + ": " + JSON.stringify(value))
+ }
+ 
+ 
+ 
+ let studentGrades = new Map();
+   studentGrades.set("John", {Grades:[{Coursename:"JavaScript", Grade:"Excellent"},{Coursename:"Jquery", Grade:"Good"},{Coursename:"CSS", Grade:"V.Good"}]});
+   studentGrades.set("Bob", {Grades:[{Coursename:"JavaScript", Grade:"Good"},{Coursename:"Jquery", Grade:"Excellent"},{Coursename:"CSS", Grade:"V.Good"}]});
+   studentGrades.set("Alice", {Grades:[{Coursename:"JavaScript", Grade:"V.Good"},{Coursename:"Jquery", Grade:"Excellent"},{Coursename:"CSS", Grade:"Good"}]});
+   
+   let studentNames = document.getElementById("studentNames");
+   studentNames.addEventListener("change", function() {
+     let studentName = studentNames.value;
+     let studentGrades = studentGrades.get(studentName);
+     console.log(studentName) 
+   }) 
+*/
